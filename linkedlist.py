db25841c6ad84ad999affb9b4aef10415e5c05ba
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-03-30 09:30:07 -0600

    Finished code for birthday, months, family, sweepstakes. adding code for Linked list

---
+++ linkedlist.py
@@ -0,0 +1,27 @@
+from node import Node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append_node(self, data):
+        node = Node(data)
+
+        def insert(self, data):
+            new_node = Node(data)
+            new_node.set_next(self.head)
+            self.head = new_node
+
+        if self.head is None:
+            self.head = node
+            return
+
+        temporary_node = self.head
+
+        while temporary_node.next is not None:
+            temporary_node = temporary_node.next
+
+        temporary_node.next = node
+
+
---
+++ node.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, data, new_next=None):
+        self.next_node = new_next
+        self.next = None
+        self.data = data
+
+    def set_next(self, new_next):
+        pass
+        
+
+
+
+
+
+
